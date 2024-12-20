96db9b5e24ce198bb4d99d2e259f871649fcee4a
mozalov <p.mozalov@dats.team>
2024-12-20 13:06:18 +0700

    feat: add webpack build + fix widget size

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,18 @@
+import './index.css';
+
+function handleError () {
+  console.log('ERROR')
+}
+
 window.onloadTurnstileCallback = function () {
   turnstile.render("#container", {
     sitekey: "0x4AAAAAAA3GsJWym_dVagkx",
     callback: function (token: string) {
       console.log(`Challenge Success ${token}`);
     },
+    language: 'en',
+    size: 'compact',
+    'expired-callback': handleError,
+    'error-callback': handleError
   });
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const webpack = require('webpack');
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 const config = {
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].bundle.[contenthash].js',
+    clean: true,
   },
   module: {
     rules: [
@@ -18,6 +22,13 @@ const config = {
         test: /\.ts(x)?$/,
         loader: 'ts-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
       }
     ]
   },
@@ -27,7 +38,15 @@ const config = {
       '.ts',
       '.js'
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+  ]
 };
 
 module.exports = config;
